36c893effc1d97930c8689f3f2700bf84aed221f
RIOguillaume95490 <guillaume.rio78@gmail.com>
2023-08-21 16:59:45 +0200

    fix error add first backend products path and check in front end

--- backend/database.sql
+++ backend/database.sql
@@ -1,141 +1,174 @@
--- MySQL dump 10.13  Distrib 8.0.32, for macos13 (arm64)
---
--- Host: localhost    Database: harryBdd
--- ------------------------------------------------------
--- Server version	8.0.32
+-- MySQL Workbench Forward Engineering
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
---
--- Current Database: `harryBdd`
---
+-- -----------------------------------------------------
+-- Schema alterworldBdd
+-- -----------------------------------------------------
 
-/*!40000 DROP DATABASE IF EXISTS `harryBdd`*/;
+-- -----------------------------------------------------
+-- Schema alterworldBdd
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `alterworldBdd` DEFAULT CHARACTER SET utf8 ;
+USE `alterworldBdd` ;
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `harryBdd` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+-- -----------------------------------------------------
+-- Table `alterworldBdd`.`utilisateurs`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `alterworldBdd`.`utilisateurs` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user-firstname` VARCHAR(250) NULL,
+  `user-lastname` VARCHAR(250) NULL,
+  `adress` VARCHAR(250) NULL,
+  `phonenumber` INT NULL,
+  `profilpicture` VARCHAR(250) NULL,
+  `password` VARCHAR(250) NULL,
+  `email` VARCHAR(250) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
-USE `harryBdd`;
 
---
--- Table structure for table `characters`
---
+-- -----------------------------------------------------
+-- Table `alterworldBdd`.`univers`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `alterworldBdd`.`univers` (
+  `idunivers` INT NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(250) NULL,
+  PRIMARY KEY (`idunivers`))
+ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `characters`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `characters` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(100) DEFAULT NULL,
-  `lastname` varchar(100) DEFAULT NULL,
-  `imgUrl` varchar(255) DEFAULT NULL,
-  `houses_id` int NOT NULL,
-  PRIMARY KEY (`id`,`houses_id`),
-  KEY `fk_characters_houses_idx` (`houses_id`),
-  CONSTRAINT `fk_characters_houses` FOREIGN KEY (`houses_id`) REFERENCES `houses` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `characters`
---
+-- -----------------------------------------------------
+-- Table `alterworldBdd`.`produits`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `alterworldBdd`.`produits` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(250) NULL,
+  `price` VARCHAR(250) NULL,
+  `imgUrl` VARCHAR(400) NULL,
+  `category` VARCHAR(100) NULL,
+  `univers` VARCHAR(100) NULL,
+  `univers_idunivers` INT NOT NULL,
+  `typeProduits` VARCHAR(100) NULL,
+  PRIMARY KEY (`id`, `univers_idunivers`),
+  INDEX `fk_produits_univers1_idx` (`univers_idunivers` ASC) VISIBLE,
+  CONSTRAINT `fk_produits_univers1`
+    FOREIGN KEY (`univers_idunivers`)
+    REFERENCES `alterworldBdd`.`univers` (`idunivers`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-LOCK TABLES `characters` WRITE;
-/*!40000 ALTER TABLE `characters` DISABLE KEYS */;
-INSERT INTO `characters` VALUES (1,'Harry','Potter','https://static.actu.fr/uploads/2023/01/25512-230106120939397-0.jpg',1),(2,'Hermione','Granger','https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Hermione_Granger_poster.jpg/220px-Hermione_Granger_poster.jpg',1),(3,'Drago','Malefoy','https://wingardium-leviosa.com/wp-content/uploads/2018/03/draco-malfoy-produit-baguette-magique.jpg',2),(5,'Ron','weasley','https://upload.wikimedia.org/wikipedia/en/thumb/5/5e/Ron_Weasley_poster.jpg/220px-Ron_Weasley_poster.jpg',1);
-/*!40000 ALTER TABLE `characters` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `houses`
---
+-- -----------------------------------------------------
+-- Table `alterworldBdd`.`supportMessage`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `alterworldBdd`.`supportMessage` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `mail` VARCHAR(250) NULL,
+  `message` VARCHAR(250) NULL,
+  `nom` VARCHAR(250) NULL,
+  `sujet` VARCHAR(250) NULL,
+  `envoi` VARCHAR(250) NULL,
+  `reception` VARCHAR(250) NULL,
+  `utilisateurs_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `utilisateurs_id`),
+  INDEX `fk_supportMessage_utilisateurs1_idx` (`utilisateurs_id` ASC) VISIBLE,
+  CONSTRAINT `fk_supportMessage_utilisateurs1`
+    FOREIGN KEY (`utilisateurs_id`)
+    REFERENCES `alterworldBdd`.`utilisateurs` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `houses`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `houses` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `houseName` varchar(100) DEFAULT NULL,
-  `housePoint` int DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `houses`
---
+-- -----------------------------------------------------
+-- Table `alterworldBdd`.`transactions`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `alterworldBdd`.`transactions` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `montant` FLOAT NULL,
+  `acheteur` VARCHAR(250) NULL,
+  `vendeur` VARCHAR(250) NULL,
+  `utilisateurs_id` INT NOT NULL,
+  `utilisateurs_commentaires_id` INT NOT NULL,
+  `utilisateurs_évaluations_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `utilisateurs_id`, `utilisateurs_commentaires_id`, `utilisateurs_évaluations_id`),
+  INDEX `fk_transactions_utilisateurs1_idx` (`utilisateurs_id` ASC, `utilisateurs_commentaires_id` ASC, `utilisateurs_évaluations_id` ASC) VISIBLE,
+  CONSTRAINT `fk_transactions_utilisateurs1`
+    FOREIGN KEY (`utilisateurs_id`)
+    REFERENCES `alterworldBdd`.`utilisateurs` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-LOCK TABLES `houses` WRITE;
-/*!40000 ALTER TABLE `houses` DISABLE KEYS */;
-INSERT INTO `houses` VALUES (1,'Grifondor',100),(2,'Serpentard',100);
-/*!40000 ALTER TABLE `houses` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `spells`
---
+-- -----------------------------------------------------
+-- Table `alterworldBdd`.`panier`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `alterworldBdd`.`panier` (
+  `user_id` INT NOT NULL,
+  `products_id` INT NOT NULL,
+  `quantity` INT NULL,
+  PRIMARY KEY (`user_id`, `products_id`),
+  INDEX `fk_user_has_products_products1_idx` (`products_id` ASC) VISIBLE,
+  INDEX `fk_user_has_products_user_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `fk_user_has_products_user`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `alterworldBdd`.`utilisateurs` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_has_products_products1`
+    FOREIGN KEY (`products_id`)
+    REFERENCES `alterworldBdd`.`produits` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `spells`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `spells` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `spellName` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `spells`
---
+-- -----------------------------------------------------
+-- Table `alterworldBdd`.`order`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `alterworldBdd`.`order` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `numCommande` INT NULL,
+  `utilisateurs_id` INT NOT NULL,
+  `prixTotal` FLOAT NULL,
+  PRIMARY KEY (`id`, `utilisateurs_id`),
+  INDEX `fk_order_utilisateurs1_idx` (`utilisateurs_id` ASC) VISIBLE,
+  CONSTRAINT `fk_order_utilisateurs1`
+    FOREIGN KEY (`utilisateurs_id`)
+    REFERENCES `alterworldBdd`.`utilisateurs` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-LOCK TABLES `spells` WRITE;
-/*!40000 ALTER TABLE `spells` DISABLE KEYS */;
-/*!40000 ALTER TABLE `spells` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `spells_has_characters`
---
+-- -----------------------------------------------------
+-- Table `alterworldBdd`.`produits_has_order`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `alterworldBdd`.`produits_has_order` (
+  `produits_id` INT NOT NULL,
+  `order_id` INT NOT NULL,
+  `quantitees` INT NULL,
+  PRIMARY KEY (`produits_id`, `order_id`),
+  INDEX `fk_produits_has_order_order1_idx` (`order_id` ASC) VISIBLE,
+  INDEX `fk_produits_has_order_produits1_idx` (`produits_id` ASC) VISIBLE,
+  CONSTRAINT `fk_produits_has_order_produits1`
+    FOREIGN KEY (`produits_id`)
+    REFERENCES `alterworldBdd`.`produits` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_produits_has_order_order1`
+    FOREIGN KEY (`order_id`)
+    REFERENCES `alterworldBdd`.`order` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `spells_has_characters`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `spells_has_characters` (
-  `spells_id` int NOT NULL,
-  `characters_id` int NOT NULL,
-  PRIMARY KEY (`spells_id`,`characters_id`),
-  KEY `fk_spells_has_characters_characters1_idx` (`characters_id`),
-  KEY `fk_spells_has_characters_spells1_idx` (`spells_id`),
-  CONSTRAINT `fk_spells_has_characters_characters1` FOREIGN KEY (`characters_id`) REFERENCES `characters` (`id`),
-  CONSTRAINT `fk_spells_has_characters_spells1` FOREIGN KEY (`spells_id`) REFERENCES `spells` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `spells_has_characters`
---
-
-LOCK TABLES `spells_has_characters` WRITE;
-/*!40000 ALTER TABLE `spells_has_characters` DISABLE KEYS */;
-/*!40000 ALTER TABLE `spells_has_characters` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-07-12  2:40:41
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
